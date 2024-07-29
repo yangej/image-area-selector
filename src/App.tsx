59283c5dae2708dbebbb93e200821db8ea321ed2
ejyang <yangej0218@gmail.com>
2024-07-29 15:11:52 +0800

    feat(app): assemble panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import styled from "@emotion/styled";
+import ImagePanel from "./components/ImagePanel/ImagePanel";
+import PreviewPanel from "./components/PreviewPanel";
+
+const Container = styled.div`
+  display: flex;
+  gap: 135px;
+`;
+
 export function App() {
-  return <div>Your code starts here</div>;
+  return (
+    <Container>
+      <ImagePanel />
+      <PreviewPanel content="test" />
+    </Container>
+  );
 }
